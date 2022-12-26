ae4aa1d5f4c801ac7d5de97cfff3cdcff898cdc9
uas-92 <uas-92@mail.ru>
2022-12-26 11:09:29 +0700

    add random to summ

--- sum/Program.cs
+++ sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int A = 2;
-int B = 178;
+﻿int A = new Random().Next(1,10);
+Console.WriteLine(A);
+int B = new Random().Next(1,10);
+Console.WriteLine(B);
 int result = A + B;
 Console.WriteLine(result);

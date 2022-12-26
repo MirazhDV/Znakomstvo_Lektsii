5e4cae8bb4b9ccda3d86831c1b24de95aeb4b39c
uas-92 <uas-92@mail.ru>
2022-12-26 11:26:11 +0700

    add if-else po imeni

---
+++ if-else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Vvedi imya ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine ("Ku ku epta ");
+}
+else
+{
+    Console.Write ("Zdarova ");
+    Console.WriteLine (username);
+}

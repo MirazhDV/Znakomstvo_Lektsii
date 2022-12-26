bf20b6286a444e64eabafa41ef9b873cbd94c4ea
uas-92 <uas-92@mail.ru>
2022-12-26 11:33:18 +0700

    add giri

---
+++ Girya/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 2;
+int b = 4;
+int c = 1;
+int d = 12;
+int e = 8;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write ("Max = ");
+Console.WriteLine (max);
